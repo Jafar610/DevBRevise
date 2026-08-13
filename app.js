0dcfb2a55c3251726c856a154d60ed61ba946495
Jafar610 <jafarmohamed955@gmail.com>
2026-08-13 18:47:45 +0300

    second commit

--- app.js
+++ app.js
@@ -1,41 +1,104 @@
-// const fs = require('fs');
-// const http = require('http');
-// const mime = require('mime-types');
-//  const path = require('path');
+import fs from "fs";
+import express from "express";
+import http from "http";
+import mime from "mime-types";
 
-
-// const server = http.createServer((req, res) => {
-//     const file = fs.readFile('./index.html', (err, data)=>{
-//         if(err){
-//             res.writeHead(404, {'Content-Type': mime.lookup(data)});
-//             res.write('File not found');
-//         } else {
-//             res.writeHead(200, {'Content-Type': mime.lookup(data)});
-//             res.write(data);
-//         }
-//     })
-// });
-
-// server.listen(3001, () =>{
-//     console.log('Server is running on port 3001');
+// fs.readFile("sample.txt", "utf8", (err, data)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(data);
+//     }
 // })
 
-const express = require('express');
+// fs.writeFile("example.txt", "This is an example text.", (err)=>{
+//     if(err) throw err;
+//     console.log("File has been saved!");
+// });
 
-const server = express();
+// fs.appendFile("sample.txt", "\nThis is a new line text added to the file.", (err)=>{
+//     if(err) throw err;
+//     console.log("file updated!");
+// });
 
+// delete file
+// fs.unlink("example.txt", (err)=>{
+//     if(err) throw err;
+//     console.log("File deleted!");
+// })
 
-server.get('/', (req, res)=>{
-    res.send('Home Page');
-})
-server.get('/about', (req, res)=>{
-    res.send('About Page');
+// create folder
+// fs.mkdir("newFolder", (err)=>{
+//     if(err) throw err;
+//     console.log("Folder created!");
+// });
+
+//file copy
+// fs.copyFile("sample.txt", "newFolder/sample.txt", (err)=>{
+//     if(err) throw err;
+//     console.log("File copied!");
+// })
+
+// read folder
+// fs.readdir("newFolder", (err, files)=>{
+//     if(err) throw err;
+//     console.log(files);
+// });
+
+// delete folder
+// fs.rmdir("newFolder", (err)=>{
+//     if(err) throw err;
+//     console.log("Folder deleted!");
+// })
+
+//   const server = express();
+//   server.use(express.json());
+
+//   server.post("/save", (req, res)=>{
+//     const user = req.body;
+//     fs.writeFile("user.json", JSON.stringify(user), (err)=>{
+//         if(err) throw err;
+//         res.send("User data saved!");
+//     })
+//   })
+
+//   server.get("/read", (req, res)=>{
+//     fs.readFile("user.json", "utf8", (err, data)=>{
+//         if(err) throw err;
+//         res.json(data);
+//     })
+//   });
+
+//   server.listen(3000, ()=>{
+//     console.log("Server is running on port 3000");
+//   })
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET") {
+    let reqUrl = req.url;
+
+    if (reqUrl === "/") {
+      reqUrl = "/index.html";
+    }
+
+    const filePath = "../barber_shop" + reqUrl;
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404, { "content-type": "text/html" });
+        res.end("<h1>404 Not Found</h1>");
+        return;
+      }
+
+      res.writeHead(200, { "content-type": mime.lookup(filePath) });
+      res.end(data);
+    });
+  }
 });
 
-server.listen(3001, (err)=>{
-    if(err){
-        console.log('Error starting server:', err);
-    }
-    console.log('Server is running on port 3001');
-})
-
+server.listen(1212, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Your server is running");
+});
